04c36c4ebace1696285b2966701f8c0e86f82170
José Pascual Gimeno Marí <tirantvcia@gmail.com>
2023-06-27 18:06:36 +0200

    refactor, change test values

--- src/test/java/com/codigosostenible/curso/data/TestData.java
+++ src/test/java/com/codigosostenible/curso/data/TestData.java
@@ -3,14 +3,14 @@ package com.codigosostenible.curso.data;
 public class TestData {
 	
 	public static final String OUPUT_TITLE_SECOND_BOOK_WITHOUT_ANCHOR = "Libro 2: 'Aprendiendo TDD en Python' por Harry Percival.";
-	public static final String OUPUT_SECOND_BOOK_REFERENCE_WITHOUT_ANCHOR = "Percival, H. (20XX). Aprendiendo TDD en Python. Editorial XYZ";
+	public static final String OUPUT_SECOND_BOOK_REFERENCE_WITHOUT_ANCHOR = "Percival, H. 20XX. Aprendiendo TDD en Python. Editorial XYZ";
 	public static final String ANCHOR_SECOND_BOOK = "[^anchor2]";
 	public static String IN_TITLE_FIRST_BOOK = "Libro 1: 'Pruebas Ágiles de Software' por Lisa Crispin y Janet Gregory. "
-			+ "[this book] (Crispin, L., & Gregory, J. (20XX). Pruebas Ágiles de Software. Editorial ABC.)";
+			+ "[this book] (Crispin, L., & Gregory, J. 20XX. Pruebas Ágiles de Software. Editorial ABC.)";
 	public static String IN_TITLE_SECOND_BOOK = "Libro 2: 'Aprendiendo TDD en Python' por Harry Percival. "
-			+ "[this book] (Percival, H. (20XX). Aprendiendo TDD en Python. Editorial XYZ)";
+			+ "[this book] (Percival, H. 20XX. Aprendiendo TDD en Python. Editorial XYZ)";
 	public static String IN_TITLE_THIRD_BOOK = "Libro 3: 'Clean Code: A Handbook of Agile Software Craftsmanship' por Robert C. Martin. "
-			+ "[this book](Martin, R. C. (20XX). Clean Code: A Handbook of Agile Software Craftsmanship. Editorial DEF.)";
+			+ "[this book](Martin, R. C. 20XX. Clean Code: A Handbook of Agile Software Craftsmanship. Editorial DEF.)";
 	
 	public static String DOCUMENT_INTRODUCTION = "Desarrollo Dirigido por Pruebas (TDD) es una metodología de desarrollo de software que se centra en escribir pruebas automatizadas antes de implementar el código.";
 	public static String ANCHOR_TEMPLATE = "[^anchorx]";
@@ -25,9 +25,9 @@ public class TestData {
 	public static String OUPUT_TITLE_THIRD_BOOK = "Libro 3: 'Clean Code: A Handbook of Agile Software Craftsmanship' por Robert C. Martin.[^anchor3]";
 	public static String THIRD_BOOK_ABSTRACT = "El TDD es una práctica esencial para escribir código limpio y bien estructurado."
 			+ "Además, el ciclo TDD proporciona una retroalimentación rápida y constante, permitiendo a los desarrolladores realizar cambios con confianza.";
-	public static String OUPUT_FIRST_BOOK_REFERENCE_WITHOUT_ANCHOR = "Crispin, L., & Gregory, J. (20XX). Pruebas Ágiles de Software. Editorial ABC.";
-	public static String OUPUT_FIRST_BOOK_REFERENCE = ANCHOR_FIRST_BOOK + ": Crispin, L., & Gregory, J. (20XX). Pruebas Ágiles de Software. Editorial ABC.";
-	public static String OUPUT_SECOND_BOOK_REFERENCE = "[^anchor2]: Percival, H. (20XX). Aprendiendo TDD en Python. Editorial XYZ";
-	public static String OUPUT_THIRD_BOOK_REFERENCE = "[^anchor3]: Martin, R. C. (20XX). Clean Code: A Handbook of Agile Software Craftsmanship. Editorial DEF.";
+	public static String OUPUT_FIRST_BOOK_REFERENCE_WITHOUT_ANCHOR = "Crispin, L., & Gregory, J. 20XX. Pruebas Ágiles de Software. Editorial ABC.";
+	public static String OUPUT_FIRST_BOOK_REFERENCE = ANCHOR_FIRST_BOOK + ": Crispin, L., & Gregory, J. 20XX. Pruebas Ágiles de Software. Editorial ABC.";
+	public static String OUPUT_SECOND_BOOK_REFERENCE = "[^anchor2]: Percival, H. 20XX. Aprendiendo TDD en Python. Editorial XYZ";
+	public static String OUPUT_THIRD_BOOK_REFERENCE = "[^anchor3]: Martin, R. C. 20XX. Clean Code: A Handbook of Agile Software Craftsmanship. Editorial DEF.";
 
 }
