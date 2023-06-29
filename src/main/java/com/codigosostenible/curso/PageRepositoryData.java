a91f5847a9feaa777932df3ad406d1f322858169
José Pascual Gimeno Marí <tirantvcia@gmail.com>
2023-06-29 23:23:28 +0200

    refector, extract insert functionality to page repository

--- src/main/java/com/codigosostenible/curso/PageRepositoryData.java
+++ src/main/java/com/codigosostenible/curso/PageRepositoryData.java
@@ -2,15 +2,60 @@ package com.codigosostenible.curso;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.IntStream;
 
 public class PageRepositoryData {
-	
-	
+
+	private static final String ANCHOR_REFERENCE_SEPARATION = ": ";
+	private static String ANCHOR_TEMPLATE = "[^anchorX]";
 	public List<String> outputLines;
 	public List<String> referenceLines;
-
+	int chpaterReferenceNumber;
+	
+	public PageRepositoryData(int chpaterReferenceNumber) {
+		this.outputLines = new ArrayList<>();
+		this.referenceLines = new ArrayList<>();
+		this.chpaterReferenceNumber = chpaterReferenceNumber;
+		
+	}
+	
 	public PageRepositoryData() {
 		this.outputLines = new ArrayList<>();
 		this.referenceLines = new ArrayList<>();
 	}
+
+	public void save(List<MarkDownOuputProcess> generatedPageFeedReferences) {
+		
+		StringBuilder outputLineWithEveryAnchorReference = new StringBuilder();
+		
+		generatedPageFeedReferences.stream().forEach(markDaown -> {
+			String sameRferenceAppearedBefore = referenceLines.stream().filter(inlineReference -> matches(inlineReference, markDaown.getPageFeedReference())).findAny().orElse(null);
+			String anchorToBePlaced = (markDaown.hasPageFeedReference)?getsAnchorReferenceToBePlace(sameRferenceAppearedBefore):"";
+			outputLineWithEveryAnchorReference.append(markDaown.getAnchorLine().concat(anchorToBePlaced));	
+			if (markDaown.hasPageFeedReference && sameRferenceAppearedBefore == null) {
+				referenceLines.add(anchorToBePlaced
+						.concat(ANCHOR_REFERENCE_SEPARATION + markDaown.getPageFeedReference()));
+			}
+		});
+		
+		outputLines.add(outputLineWithEveryAnchorReference.toString());
+
+	}
+
+	private String getsAnchorReferenceToBePlace(String sameRferenceAppearedBefore) {
+		return (sameRferenceAppearedBefore != null)? sameRferenceAppearedBefore.substring(0, sameRferenceAppearedBefore.indexOf(":")):ANCHOR_TEMPLATE.replace("X", String.valueOf(referenceLines.size() + 1));
+	}
+
+
+	private boolean matches(String referenceLine, String pageFeedReference) {
+
+		return referenceLine.contains(pageFeedReference);
+	}
+
+	public List<MarkDownOuputProcess> findAll() {
+		return null;
+		// TODO Auto-generated method stub
+
+	}
+
 }
--- src/main/java/com/codigosostenible/curso/PageService.java
+++ src/main/java/com/codigosostenible/curso/PageService.java
@@ -3,12 +3,9 @@ package com.codigosostenible.curso;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.stream.IntStream;
 
 public class PageService {
 
-	private static final String ANCHOR_REFERENCE_SEPARATION = ": ";
-	private static String ANCHOR_TEMPLATE = "[^anchorX]";
 	TransformationMarkdownProcess markDownProcess;
 	PageRepositoryData repository;
 
@@ -20,41 +17,15 @@ public class PageService {
 
 	}
 
-	public void add(String text) {
-		List<MarkDownOuputProcess> generatedPageFeedReference = markDownProcess.generatePageFeedReference(text);
-		StringBuilder outputLineWithAllMarkDownResults = new StringBuilder();
+	public void add(String sourceLine) {
+		List<MarkDownOuputProcess> generatedPageFeedReference = markDownProcess.generatePageFeedReference(sourceLine);
 		
-		for(int i = 0; i < generatedPageFeedReference.size(); i++) {
-			String anchorToBePlaced = "";
-			if(generatedPageFeedReference.get(i).isHasPageFeedReference()) {
-				int position = findReferenceIntoReferenceLines(generatedPageFeedReference.get(i).getPageFeedReference());
-				anchorToBePlaced = (isReferenceInPageFeed(position))?ANCHOR_TEMPLATE.replace("X", String.valueOf(position + 1)):ANCHOR_TEMPLATE.replace("X", String.valueOf(repository.referenceLines.size() + 1));
-				if(!isReferenceInPageFeed(position)) {
-					repository.referenceLines.add(anchorToBePlaced.concat(ANCHOR_REFERENCE_SEPARATION+generatedPageFeedReference.get(i).getPageFeedReference()));	
-				}
-				
-				
-			}
-			outputLineWithAllMarkDownResults.append(generatedPageFeedReference.get(i).getAnchorLine().concat(anchorToBePlaced));
-			
-			
-		}
-		this.repository.outputLines.add(outputLineWithAllMarkDownResults.toString());
-	}
-
-	private int findReferenceIntoReferenceLines(String pageFeedReference) {
-		return IntStream.range(0, repository.referenceLines.size()).filter(i -> matches(repository.referenceLines.get(i), pageFeedReference)).findFirst().orElse(-1);
-	}
-
-	private boolean isReferenceInPageFeed(int position) {
-		return position != -1;
-	}
-
-
-	private boolean matches(String referenceLine, String pageFeedReference) {
+		repository.save(generatedPageFeedReference);
 		
-		return referenceLine.contains(pageFeedReference);
+
+		
 	}
+
 
 	public List<String> allLines() {
 		List<String> lines = new ArrayList<>();
